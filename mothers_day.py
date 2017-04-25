b6b6e3765db813a12ffc92deec1583fc06cf1507
Zack Spann <zspann@zspann-macbookpro3.roam.corp.google.com>
2017-04-25 15:13:39 -0700

    all tests passing

---
+++ mothers_day.py
@@ -0,0 +1,11 @@
+def mothers_day():
+    return "Happy Mother's Day, Mom!"
+
+def better_mothers_day(name):
+    return "Happy Mother's Day, " + name + "!"
+
+def best_mothers_day(name = "Mom"):
+    return "Happy Mother's Day, " + name + "!"
+
+def holiday_greeting(to = "Mom", sender = "Your Favorite Child", holiday = "Mother's Day"):
+    return "Happy " + holiday + ", " + to + "! - From " + sender
